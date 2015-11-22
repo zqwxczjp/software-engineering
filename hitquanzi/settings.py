c62c22412b5f4d1df85c1c11e2acc16f59e9c5f5
HITZJP <254659389@qq.com>
2015-11-22 09:43:55 +0800

    sign up sign in

--- hitquanzi/settings.py
+++ hitquanzi/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'hitquanzi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'quanzi',
-        'USER': 'root',
-        'PASSWORD': '1026',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': 'quanzi',
+        #'USER': 'root',
+        #'PASSWORD': '1026',
     }
 }
 
